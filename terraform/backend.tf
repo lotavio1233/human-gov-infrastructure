d464ce8fa91669cce5e911a2ca6bf8488aaf0ca7
lotavio123 <mm2r1000@gmail.com>
2024-11-15 20:48:43 +0000

    Added IAM Role to terraform module

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "humangov-terraform-state-lol921"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "humangov-terraform-state-lock-table"
-  }
-}
