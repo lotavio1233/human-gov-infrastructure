bfe31b07f6c88f46e29b9ccc8d1d33ada210d58f
lotavio1233 <mm2r1000@gmail.com>
2024-11-24 19:07:03 +0000

    Added variable and provisioners to terraform module

--- terraform/modules/aws_humangov_infrastructure/main.tf
+++ terraform/modules/aws_humangov_infrastructure/main.tf
@@ -36,7 +36,7 @@ resource "aws_security_group" "state_ec2_sg" {
 }
 
 resource "aws_instance" "state_ec2" {
-  ami           = "ami-0866a3c8686eaeeba"
+  ami           = "ami-005fc0f236362e99f"
   instance_type = "t2.micro"
   key_name = "humangov-ec2-key"
   vpc_security_group_ids = [aws_security_group.state_ec2_sg.id]
